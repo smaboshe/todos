10397278be7f40536504d55739dc978be360361e
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-04-29 09:55:46 +0200

    Allow creation of todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :todos, only: [:create, :new]
+
   root "todos#index"
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+
+    click_on "Add a new todo"
+    task_todo = "Buy bread"
+    fill_in "Title", with: task_todo
+    click_on "Create Todo"
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_css ".todos li", text: task_todo
+  end
+end
